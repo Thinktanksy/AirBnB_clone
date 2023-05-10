521d500122b34217c684a529883504ae0ceffbac
Joseph Williams <richlike.joseph@gmail.com>
2023-05-09 23:10:53 -0700

    Airbnb Clone

---
+++ models/city.py
@@ -0,0 +1,10 @@
+s module creates a User class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for managing city objects"""
+
+    state_id = ""
+    name = ""
